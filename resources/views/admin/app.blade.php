10f2ddf9ca6b32ac1ccfb1c68260372700114a10
ramsonrajagukguk <ramsonmuara@gmail.com>
2022-02-10 01:11:07 +0700

    Part admin

---
+++ resources/views/admin/app.blade.php
@@ -0,0 +1,44 @@
+<!--
+=========================================================
+* Soft UI Dashboard - v1.0.3
+=========================================================
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <title>
+    Soft UI Dashboard by Creative Tim
+  </title>
+  <!-- Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="{{ url('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+  <link href="{{ url('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="{{ url('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{ url('assets/css/soft-ui-dashboard.css?v=1') }}" rel="stylesheet" />
+  <!-- Alpine -->
+  {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script> --}}
+</head>
+
+<body class="g-sidenav-show  bg-gray-100">
+ 
+    @include('admin.layouts.navbars.nav')
+  <!-- Left side column. contains the logo and sidebar -->
+    @include('admin.layouts.navbars.sidebar')
+
+    <section class="content">
+        @yield('content')
+      </section>
+   
+      @include('admin.layouts.footers.footer')
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('beranda');
+    return view('admin.beranda');
 });
 
 Auth::routes(['verify' => true]);
